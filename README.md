896ec13b24b37cc86297b90a64c7aebf0812d172
Gary Chisholm <garyjchisholm@gmail.com>
2018-09-11 20:14:30 +1000

    Migrated more stuff to brew, fixed a few things

--- README.md
+++ README.md
@@ -1,33 +1,20 @@
 ## Gappling Gun :gun:
 
-Gappling Gun is an automated app installer for MacOS.
+Gappling Gun is an automated app installer for MacOS that sets up everyting I need.
 
-It bootstraps a fresh Mac install with basic apps and utils via brew, npm, github, and app installs.
-
-A bash script that takes in an array of URLs, downloads the `.dmg`, `.zip`, `.tbz2`, or `.bz2` files, and then installs their `.app`/`.pkg`.
+git, node, brew, ohmyzsh, firefox and chrome amongst others
 
 ## Automate :robot:
 
-via `curl`
+One liner magic :sparkles:
 
 ```
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/omgaz/gappling-gun/master/run.sh)"
 ```
 
-If you have git clone the repo, and run the gappling gun:
-
-```shell
-git clone https://github.com/omgaz/gappling-gun
-cd gappling-gun
-chmod +x ./run.sh
-./run.sh
-```
-
 ## Relax :coffee:
 
 > Go grab a coffee
-
-Don't worry if a bunch of windows appear, let it do its thing, it'll unmount when its done
 
 ## What's included
 
@@ -55,10 +42,8 @@ Don't worry if a bunch of windows appear, let it do its thing, it'll unmount whe
 * NVM
 * Oh My Zsh
 
-## Known Issues :bug:
+## What's not included
 
-Sometimes Google Chrome asks to accept T&C on install, you can just `:q` and `Y` to accept
+Warranty, you're responsible for the code you run on your machine, not me.
 
-I don't delete any of the install files (in case they fail you can install them manually), but you may need to clean-up when you're done.
-
-Install URLs are recent as at commit. This should probably be extracted for easier updating and maintenance.
+And once again, happy hacking :)
--- run.sh
+++ run.sh
@@ -1,81 +1,52 @@
 # #!/bin/bash
+echo "For git setup what's your name:"
+read name
+echo "… and your email address?"
+read email
+echo "Cool, and your password for those sudo commands:"
+sudo -v
+echo "Awesome, let's go!"
 
-typora='https://typora.io/download/Typora.dmg'
-licecap='https://www.cockos.com/licecap/licecap128.dmg'
-chrome='https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
-st3='https://download.sublimetext.com/Sublime%20Text%20Build%203176.dmg'
-firefox='https://download-installer.cdn.mozilla.net/pub/firefox/releases/61.0.2/mac/en-GB/Firefox%2061.0.2.dmg'
-postbox='https://d3nx85trn0lqsg.cloudfront.net/mac/postbox-6.1.1-mac64.dmg'
-vbox='https://download.virtualbox.org/virtualbox/5.2.16/VirtualBox-5.2.16-123759-OSX.dmg'
-slack='https://downloads.slack-edge.com/mac_releases/Slack-3.2.0.dmg'
+sudo chown -R $(whoami) /usr/local
 
-githubdesktop='https://desktop.githubusercontent.com/releases/1.3.2-ed5395e6/GitHubDesktop.zip'
-beyondcompare='https://www.scootersoftware.com/BCompareOSX-4.2.6.23150.zip'
-mater='https://github.com/jasonlong/mater/releases/download/v1.0.3/Mater-darwin-x64.zip'
-ubar='https://brawersoftware.com/downloads/ubar/ubar.zip'
-appcleaner='https://freemacsoft.net/downloads/AppCleaner_3.4.zip'
-gitup='https://s3-us-west-2.amazonaws.com/gitup-builds/stable/GitUp.zip'
-vscode='https://az764295.vo.msecnd.net/stable/1dfc5e557209371715f655691b1235b6b26a06be/VSCode-darwin-stable.zip'
-
-imageoptim='https://imageoptim.com/ImageOptim.tbz2'
-imagealpha='https://pngmini.com/ImageAlpha1.5.1.tar.bz2'
-
-
-declare -a apps=($vbox $ubar $appcleaner $vscode $gitup $typora $st3 $postbox $licecap $imageoptim $imagealpha $slack $githubdesktop $firefox $chrome $beyondcompare $mater)
+sudo xcode-select --reset
 
 echo "→ Installing Homebrew Packages…"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew install git graphicsmagick
-brew cask install hyper font-hack font-montserrat font-roboto font-roboto-mono
+brew tap homebrew/cask-fonts
+brew install git graphicsmagick node
+brew cask install github virtualbox gitup ubar typora appcleaner sublime-text slack licecap postbox google-chrome firefox beyond-compare hyper font-hack font-montserrat font-roboto font-roboto-mono visual-studio-code imageoptim imagealpha vlc java
 
-echo "↓ Downloading…"
-for appurl in "${apps[@]}"; do
-  echo $appurl
-  if ! curl -# -O $appurl; then
-    printf 'Curl failed with error code "%d" (check the manual)\n' "$?" >&2
-  fi
-done
-
-echo "→ Installing Zips…"
-
-# ubar, mater, beyond-compare, github desktop, app cleaner, gitup, vscode
-unzip -q *.zip
-
-# imageoptim, imagealpha
-tar jxf  *.tbz2
-tar jxf  *.bz2
-mv *.app /Applications
-
-echo "→ Mounting Archives…"
-# chrome, licecap, typora, firefox, sublime text
-for dmg in *.dmg; do
-  hdiutil attach -quiet "$dmg"
-done
-
-echo "→ Installing Apps…"
-for app in /Volumes/*/*.app; do
-  appdirname="$(dirname "$app")"
-  cp -n -R "${app}" /Applications
-  hdiutil unmount -quiet "$appdirname"
-done
-
-echo "→ Installing Packages…"
-# virtualbox
-for pkg in /Volumes/*/*.pkg; do
-  pkgdirname="$(dirname "$pkg")"
-  echo $pkgdirname
-  sudo installer -package $pkg -target "/Volumes/Macintosh HD"
-  hdiutil unmount -quiet "$pkgdirname"
-done
-
-echo "→ Installing NodeJS…"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-echo "write-good\nalex\nproselint\npure-prompt\nserve\nsvgo\nfast-cli" >> $NVM_DIR/default-packages
-nvm install --lts=carbon
-nvm use --lts=carbon
+echo "→ Configuring Git…"
+git config --global user.name $name
+git config --global user.email $email
+git config --global credential.helper osxkeychain
 
 echo "→ Installing Oh My Zsh…"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://gist.githubusercontent.com/omgaz/61819cd3f8e9c19cdee8cffdfb794cac/raw/81f5e0268814bcefc66b26e3011d5d39b278d2a9/.zshrc >> ~/.zshrc
+zsh
+
+echo "→ Installing NodeJS/NPM stuff…"
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+npm i -g n npm@latest
+n lts
+rm -rf ~/n/bin/npm
+ln -s ~/.npm-global/bin/npm ~/n/bin/npm
+npm i -g write-good alex eslint pure-prompt serve svgo fast-cli
+
+materVer=$(curl https://api.github.com/repos/jasonlong/mater/releases/latest -s \
+  | grep tag_name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+echo "→ Installing Mater $materVer…"
+curl -# -L -O https://github.com/jasonlong/mater/releases/download/$materVer/Mater-darwin-x64.zip
+unzip -q *.zip
+mv *.app /Applications
+rm -rf Mater-darwin-x64.zip
 
 echo "✓ Complete."
